a905738f67ad13fb010c765e9f235d9524e64cd2
rohit <rohit7310kr@gmail.com>
2026-05-04 16:13:24 +0530

    bugs: deploy bug fixed

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,7 @@ dotenv.config();
 
 connectDB();
 const app = express();
-app.use(
-  cors({
-    origin: "https://onrender.com",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  }),
-);
+app.use(cors());
 
 app.use(express.json());
 
